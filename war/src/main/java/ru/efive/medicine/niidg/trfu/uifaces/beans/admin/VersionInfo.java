3a48f37fb18a79b458a3362d0f689242342bf083
viktor morozov <viktor.morozov@iqubecompany.com>
2013-07-30 15:07:54 +0300

    TRFU-45, версия изменена с 0.3.17 на 0.3.18

---
+++ war/src/main/java/ru/efive/medicine/niidg/trfu/uifaces/beans/admin/VersionInfo.java
@@ -0,0 +1,29 @@
+package ru.efive.medicine.niidg.trfu.uifaces.beans.admin;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Named;
+
+@Named("versionInfo")
+@ConversationScoped
+public class VersionInfo implements Serializable {
+	private static final long serialVersionUID = 6929693412341369918L;
+	private String versionAssembly = "0.3.18";
+	private String dateAssembly = "30.07.2013 16:07:00";
+	
+	public String getVersionAssembly() {
+		return versionAssembly;
+	}
+	public void setVersionAssembly(String versionAssembly) {
+		this.versionAssembly = versionAssembly;
+	}
+	public String getDateAssembly() {
+		return dateAssembly;
+	}
+	public void setDateAssembly(String dateAssembly) {
+		this.dateAssembly = dateAssembly;
+	}
+	
+	
+}
