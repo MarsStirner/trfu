4bb6d2e2e1f9d11d0290ecb6b1eba6b122beb09e
viktor morozov <viktor.morozov@iqubecompany.com>
2013-08-02 15:53:37 +0300

    TRFU-48, версия изменена с 0.3.20 на 0.3.21

--- war/src/main/java/ru/efive/medicine/niidg/trfu/uifaces/beans/admin/VersionInfo.java
+++ war/src/main/java/ru/efive/medicine/niidg/trfu/uifaces/beans/admin/VersionInfo.java
@@ -9,8 +9,8 @@ import javax.inject.Named;
 @ConversationScoped
 public class VersionInfo implements Serializable {
 	private static final long serialVersionUID = 6929693412341369918L;
-	private String versionAssembly = "0.3.20";
-	private String dateAssembly = "02.08.2013 13:07:00";
+	private String versionAssembly = "0.3.21";
+	private String dateAssembly = "02.08.2013 16:55:00";
 	
 	public String getVersionAssembly() {
 		return versionAssembly;
