5fc33215f54649764ae68b6b3741763fa82b310f
viktor morozov <viktor.morozov@iqubecompany.com>
2013-10-25 15:18:36 +0300

    TRFU-94, версия изменена с 0.3.55 на 0.3.56

--- war/src/main/java/ru/efive/medicine/niidg/trfu/uifaces/beans/admin/VersionInfo.java
+++ war/src/main/java/ru/efive/medicine/niidg/trfu/uifaces/beans/admin/VersionInfo.java
@@ -9,8 +9,8 @@ import javax.inject.Named;
 @ConversationScoped
 public class VersionInfo implements Serializable {
 	private static final long serialVersionUID = 6929693412341369918L;
-	private String versionAssembly = "0.3.55";
-	private String dateAssembly = "25.10.2013 13:30:00";
+	private String versionAssembly = "0.3.56";
+	private String dateAssembly = "25.10.2013 16:20:00";
 	
 	public String getVersionAssembly() {
 		return versionAssembly;
