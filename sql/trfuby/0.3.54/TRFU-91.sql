af134a45623630c6220854c54d5d6e08d17b44c4
EkVoronina <katia85@mail.ru>
2014-07-25 17:18:02 +0400

    Create table

--- sql/trfuby/0.3.54/TRFU-91.sql
+++ sql/trfuby/0.3.54/TRFU-91.sql
@@ -1,4 +1,23 @@
-INSERT INTO `trfu`.`roles` (`id`, `name`, `roleType`) VALUES ('14', 'Вирусинактивация', 'VIRUSINACTIVATION');
+﻿INSERT INTO `trfu`.`roles` (`id`, `name`, `roleType`) VALUES ('14', 'Вирусинактивация', 'VIRUSINACTIVATION');
+
+CREATE TABLE `trfu`.`trfu_dir_additionalLiquor` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`uuid` VARCHAR(255) NULL DEFAULT NULL,
+	`additionalVolume` INT(11) NOT NULL,
+	`deleted` BIT(1) NOT NULL,
+	`additionalLiquor_id` INT(11) NULL DEFAULT NULL,
+	`componentType_id` INT(11) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE INDEX `componentType_id` (`componentType_id`),
+	INDEX `FK2959F7AF48905E06` (`componentType_id`),
+	INDEX `FK2959F7AFD444F476` (`additionalLiquor_id`),
+	CONSTRAINT `FK2959F7AF48905E06` FOREIGN KEY (`componentType_id`) REFERENCES `trfu_blood_component_types` (`id`),
+	CONSTRAINT `FK2959F7AFD444F476` FOREIGN KEY (`additionalLiquor_id`) REFERENCES `trfu_classifiers` (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=0;
+
 
 INSERT INTO trfu.trfu_dir_additionalLiquor (`id`, `uuid`, `additionalVolume`, `componentType_id`, `additionalLiquor_id`, `deleted`) 
 VALUES (1, null, 35, 108, 111, 0);
