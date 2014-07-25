aa633e3c37a2517f2f85cc9f09713cc3a26001f4
EkVoronina <katia85@mail.ru>
2014-07-25 17:59:07 +0400

    Create columns 2

--- sql/trfuby/0.3.98/TRFU-120.sql
+++ sql/trfuby/0.3.98/TRFU-120.sql
@@ -4,10 +4,10 @@ ADD COLUMN	`additionalLiquor_id` INT(11) NULL DEFAULT NULL after `additionalVolu
 ADD COLUMN	`bloodSystem_id` INT(11) NULL DEFAULT NULL after `additionalLiquor_id`;
 
 
-CREATE	INDEX `FK56F2D4AD6151C161` ON  `trfu_blood_components`(`bloodSystem_id`);
-CREATE 	INDEX `FK56F2D4ADD444F476` ON `trfu_blood_components`(`additionalLiquor_id`);
+CREATE	INDEX `FK56F2D4AD6151C161` ON `trfu`.`trfu_blood_components`(`bloodSystem_id`);
+CREATE 	INDEX `FK56F2D4ADD444F476` ON `trfu`.`trfu_blood_components`(`additionalLiquor_id`);
 
-ALTER TABLE `trfu_blood_components` ADD	CONSTRAINT `FK56F2D4AD6151C161` FOREIGN KEY (`bloodSystem_id`) REFERENCES `trfu_blood_systems` (`id`);
-ALTER TABLE `trfu_blood_components` ADD	CONSTRAINT `FK56F2D4ADD444F476` FOREIGN KEY (`additionalLiquor_id`) REFERENCES `trfu_classifiers` (`id`);
+ALTER TABLE `trfu`.`trfu_blood_components` ADD	CONSTRAINT `FK56F2D4AD6151C161` FOREIGN KEY (`bloodSystem_id`) REFERENCES `trfu_blood_systems` (`id`);
+ALTER TABLE `trfu`.`trfu_blood_components` ADD	CONSTRAINT `FK56F2D4ADD444F476` FOREIGN KEY (`additionalLiquor_id`) REFERENCES `trfu_classifiers` (`id`);
 
 ALTER TABLE `trfu`.`trfu_blood_components` ADD COLUMN `bigLabelPath` VARCHAR(255) NULL AFTER `bloodSystem_id`;
