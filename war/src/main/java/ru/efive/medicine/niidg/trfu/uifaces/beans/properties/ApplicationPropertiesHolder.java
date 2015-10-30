095de60d816afaaedd5335cc0e6a0823dd603a89
Upatov Egor <EUpatov@korusconsulting.ru>
2015-10-30 19:41:49 +0300

    properties file  path store  in system variable (TODO file must be include in package)

--- war/src/main/java/ru/efive/medicine/niidg/trfu/uifaces/beans/properties/ApplicationPropertiesHolder.java
+++ war/src/main/java/ru/efive/medicine/niidg/trfu/uifaces/beans/properties/ApplicationPropertiesHolder.java
@@ -1,6 +1,9 @@
 package ru.efive.medicine.niidg.trfu.uifaces.beans.properties;
 
+import org.apache.commons.lang.StringUtils;
+import ru.efive.medicine.niidg.trfu.context.NoPropertyStoragePathException;
 import ru.efive.medicine.niidg.trfu.uifaces.beans.IndexManagementBean;
+import ru.efive.medicine.niidg.trfu.uifaces.beans.properties.util.PropertyTypeNotSupported;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
@@ -8,11 +11,11 @@ import javax.ejb.Startup;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.ParseException;
+import java.util.*;
 
 @Singleton
 @Startup
@@ -28,22 +31,36 @@ public class ApplicationPropertiesHolder {
     private transient IndexManagementBean contextManagementBean;
 
     @PostConstruct
-    public void init(){
+    public void init() throws IOException, NoPropertyStoragePathException, PropertyTypeNotSupported, ParseException {
+        storagePath = System.getProperty("efive.trfu.home");
+        if (StringUtils.isEmpty(storagePath)) {
+            throw new NoPropertyStoragePathException();
+        }
 
-//        File dir = new File(storagePath+"static/");
-//        List<String> fileNames = Arrays.asList(dir.list(filter));
-//        for (String fileName: fileNames) {
-//            properties.put(fileName.split(".properties")[0], new ExtendedProperties(storagePath + "static/" + fileName));
-//            isStatic.put(fileName.split(".properties")[0], true);
-//        }
-//
-//        dir = new File(storagePath+"dynamic/");
-//        fileNames = Arrays.asList(dir.list(filter));
-//
-//        for (String fileName: fileNames) {
-//            properties.put(fileName.split(".properties")[0], new ExtendedProperties(storagePath + "dynamic/" + fileName));
-//            isStatic.put(fileName.split(".properties")[0], false);
-//        }
+        if(!storagePath.endsWith("/")){
+            storagePath+="/";
+        }
+        FilenameFilter filter = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".properties");
+            }
+        };
+
+        File dir = new File(storagePath+"static/");
+        List<String> fileNames = Arrays.asList(dir.list(filter));
+        for (String fileName: fileNames) {
+            properties.put(fileName.split(".properties")[0], new ExtendedProperties(storagePath + "static/" + fileName));
+            isStatic.put(fileName.split(".properties")[0], true);
+        }
+
+        dir = new File(storagePath+"dynamic/");
+        fileNames = Arrays.asList(dir.list(filter));
+
+        for (String fileName: fileNames) {
+            properties.put(fileName.split(".properties")[0], new ExtendedProperties(storagePath + "dynamic/" + fileName));
+            isStatic.put(fileName.split(".properties")[0], false);
+        }
     }
 
     public Object getProperty(String fileAlias, String key) {
