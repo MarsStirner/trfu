1b733517c6d4963e0094ec367c64d57813dcaab1
Upatov Egor <EUpatov@korusconsulting.ru>
2016-07-18 16:27:07 +0300

    Remove unused alfresco module

--- alfresco-dao/src/test/java/ru/efive/dao/alfresco/test/AlfrescoHelper.java
+++
@@ -1,8 +0,0 @@
-package ru.efive.dao.alfresco.test;
-
-public final class AlfrescoHelper {
-	
-	public static final String TEST_RUNTIME_USERNAME = "admin";
-	public static final String TEST_RUNTIME_PASSWORD = "admin";
-	
-}
